9eb01327b6e8547948bd9819ab7ba2014ac467ee
Brittany Powell <armybritbrat@gmail.com>
2023-10-26 08:09:09 -0400

    HELLO WORLD

--- my-first-ops-challenge.sh
+++ my-first-ops-challenge.sh
@@ -1,1 +1,21 @@
-This is our first ACP workflow
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Brittany Powell
+# Date of latest revision:      10/26/2023
+# Purpose:                      print something to the screen
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+
+echo "HELLO WORLD"
+
+
+
+
+# End
