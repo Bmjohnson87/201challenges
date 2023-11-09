cd8772c48a0febac7ddfb23a45f0fe26b0273401
Brittany Powell <armybritbrat@gmail.com>
2023-11-09 15:36:22 -0500

    testpush

---
+++ domain_info.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# telling the user to enter a domain name
+echo "Please type in a domain"
+# read the user input and put it the domain variable
+read $domain
+# commands to be used by the script
+#  whois|dig|host|nslookup
+
+dns-info() {
+    whois $domain
+    dig $domain
+    host $domain
+    nslookup $domain
+}
+
+
+#
+
+# call the function
+dns-info 
